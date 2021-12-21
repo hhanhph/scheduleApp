88f6d8fbb47aaf31ba98a638c7c417a8e7868483
Hanh Phan <hanh.phan@brandung.de>
2021-12-21 21:20:04 +0100

    Check time when img loaded

--- cypress/integration/schedule.spec.js
+++ cypress/integration/schedule.spec.js
@@ -20,6 +20,29 @@ describe("Homepage", () => {
             const duration = measure.duration;
             assert.isAtMost(duration, 5000);
           });
+        cy.get(
+          '*[class="datepicker-date-day-Item wrapper  date-day-Item-selected"]'
+        )
+          .first()
+          .click()
+          .then(() => {
+            cy.get("img")
+              .first()
+              .should("be.visible")
+              .then(() => performance.mark("load-first-img"))
+              .then(() => {
+                performance.measure(
+                  "imgLoad",
+                  "start-loading",
+                  "load-first-img"
+                );
+                // Retrieve the timestamp we just created
+                const measure2 = performance.getEntriesByName("imgLoad")[0];
+                // This is the total amount of time (in milliseconds) between the start and end
+                const duration = measure2.duration;
+                assert.isAtMost(duration, 5000);
+              });
+          });
       });
   });
 });
